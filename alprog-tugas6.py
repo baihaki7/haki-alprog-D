d4daa3fefa83de2c7b01293516eee2f261bd5e64
baihaki7 <haki000015@gmail.com>
2024-11-17 01:52:45 +0800

    Add files via upload

---
+++ alprog-tugas6.py
@@ -0,0 +1,14 @@
+# Modifikasi Tugas 2
+
+x = 28
+y = 24
+luas = 672
+
+x = input("masukan nilai x:")
+print("nilai x adalah :",x)
+
+y = input("masukan nilai y:")
+print("nilai y adalah :",y)
+
+luas = input("masukan nilai luas:")
+print("nilai luas adalah :",luas)
