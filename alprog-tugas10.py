8a0d6de14a920cd9cb8875718f6a36f433f44cdb
baihaki7 <haki000015@gmail.com>
2025-01-05 10:10:37 +0700

    Add files via upload

---
+++ alprog-tugas10.py
@@ -0,0 +1,37 @@
+# perbandingan - jawaban ada 2 ture false
+# >,  <, >=,  <=, ==,  !=, is, isnot
+
+x = 5
+y= 10
+
+# 1.apakah x lebih besar dari y ?
+hasil= x > y
+print("x > y=",hasil)
+
+# 2. Apakah x lebih kecil dari y?
+hasil = x < y
+print("x < y =", hasil)
+
+# 3.apakah x lebih besar atau sama dengan y?
+hasil= x >= y
+print("x >= y", hasil)
+
+# 4. Apakah x lebih kecil atau sama dengan y?
+hasil = x <= y
+print("x <= y =", hasil)
+
+# 5. Apakah x sama dengan 5?
+hasil = x == 5
+print("x == 5 =", hasil) 
+
+# 6. Apakah y tidak sama dengan 10?
+hasil = y != 10
+print("y != 10 =", hasil) 
+
+# 7. Apakah x adalah objek yang sama dengan y?
+hasil = x is y
+print("x is y =", hasil) 
+
+# 8. Apakah x bukan objek yang sama dengan y?
+hasil = x is not y
+print("x is not y =", hasil)
